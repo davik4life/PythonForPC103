23ff6a01af7eb2fd97617cda27a2dcee684da2b7
Victor Adeshile <davik4life@gmail.com>
2023-09-13 12:53:34 +0100

    Added a custom string formating style to concatenate the string.

--- favColor.py
+++ favColor.py
@@ -1,2 +1,6 @@
 favColor = input("Pls type your favourite color: ")
-print("Your favorite color is:\n" + favColor)
+# print("Your favorite color is:\n" + favColor)
+
+# This uses the string formating style. Allow for neat and easy concatenation.
+output = f"Your favorite color is:\n{favColor}"
+print(output)
