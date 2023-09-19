a0f14b2a2a1c9492d1758e9dae344aae90816609
Victor Adeshile <davik4life@gmail.com>
2023-09-19 21:51:59 +0100

    Added a shapecompute.py script

---
+++ shapesCompute.py
@@ -0,0 +1,21 @@
+# Square—The area is the length of a side squared.
+#  Square = sideValue * sideValue
+
+squareArea = input("What is the length of a side of the square?: ")
+parsedSquareArea = float(squareArea) * float(squareArea)
+print(f"The area of the square is: {parsedSquareArea}")
+
+# Rectangle—The area is the length multiplied by the width.
+# Rectangle = lengthValue * widthValue
+
+rectangleLength = input("What is the length of a side of the Rectangle?: ")
+rectangleWidth = input("What is the width of a side of the Rectangle?: ")
+parsedRectangleArea = float(rectangleLength) * float(rectangleWidth)
+print(f"The area of the Rectangle is: {parsedRectangleArea}")
+
+# Circle—The area is Pi (you can use 3.14) multiplied by the radius squared.
+# Circle = Pi 3.14 * radiusSquaredValue
+
+circleArea = input("What is the radius of the circle?: ")
+parsedCircleArea = 3.14 * (float(circleArea) ** 2)
+print(f"The area of the circle is: {parsedCircleArea}")
