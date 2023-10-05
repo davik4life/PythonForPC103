69d6d073d61f92fe342f327e0168dc7481802b7a
Victor Adeshile <davik4life@gmail.com>
2023-10-05 13:05:16 +0100

    Added forLoop.py

---
+++ forLoop.py
@@ -0,0 +1,20 @@
+# Display the list in order
+
+colors = ["red", "blue", "green", "yellow"]
+
+print()
+
+for color in colors:
+    print(color)
+
+print()
+
+# Display the number 1 -8
+for i in range(1, 9):
+    print(i)
+
+print()
+
+# Display the even numbers within 2-20
+for i in range(2, 21, 2):
+    print(i)
