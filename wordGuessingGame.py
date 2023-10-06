488a1c8dd38ab7c69fa01d258885a52d8f622362
Victor Adeshile <davik4life@gmail.com>
2023-10-06 02:23:40 +0100

    Added the first milestone for wordGuessingGame.py

---
+++ wordGuessingGame.py
@@ -0,0 +1,11 @@
+secretWord = "victor"
+guessWord = ""
+guessCount = 0
+while secretWord != guessWord:
+    guessWord = input("What is your guess? ").lower()
+    guessCount += 1
+    if secretWord != guessWord:
+        print("Your guess was not correct.")
+    else:
+        print("Congratulations! You guessed it")
+        print(f"It took you {guessCount} guess(es)")
