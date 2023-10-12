079896bb9c2e909243163a4cd3606f4443e0af6a
Victor Adeshile <davik4life@gmail.com>
2023-10-12 03:28:21 +0100

    Added seriesOfNumbers.py script

---
+++ seriesOfNumbers.py
@@ -0,0 +1,24 @@
+# Ask for series of Numbers from the user.
+numbers = []
+keepAsking = True
+sumOfNum = 0
+averageNum = 0
+largestNumber = 0
+
+while keepAsking:
+    numberEntries = int(
+        input("Enter a list of numbers, type 0 when finished.: "))
+    if numberEntries != 0:
+        numbers.append(numberEntries)
+        sumOfNum += numberEntries
+        averageNum = sumOfNum / len(numbers)
+    else:
+        print()
+        for number in numbers:
+            if number > largestNumber:
+                largestNumber = number
+            print(f"Enter number: {number}")
+        print(f"The sum is: {sumOfNum}")
+        print(f"The average is: {averageNum}")
+        print(f"The largest number is: {largestNumber}")
+        keepAsking = False
